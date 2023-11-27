9541107740c1d039522d3624a76e51dcc211ab42
Austin Graham <austiingraham@gmail.com>
2023-11-27 00:06:18 -0500

    added functoin that returns license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,9 +7,14 @@ function renderLicenseBadge(license) {
   return `[![License](https://img.shields.io/badge/license-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `[License](https://opensource.org/licenses/${license})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
