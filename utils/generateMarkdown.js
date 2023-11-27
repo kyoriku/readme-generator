2d915f585cec61fe87ec9028a8024eb32c0cbdf2
Austin Graham <austiingraham@gmail.com>
2023-11-27 01:02:06 -0500

    added function that returns license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,9 +16,14 @@ function renderLicenseLink(license) {
   return `[License](https://opensource.org/licenses/${license})`;
 }
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `This application is covered by the [${license}](https://opensource.org/licenses/${license}) license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
